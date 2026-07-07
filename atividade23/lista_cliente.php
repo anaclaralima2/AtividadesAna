ca7b1ae0e6d84545d5f98ad4c818377176e5bfb0
anaclaralima2 <anaclara98288671@gmail.com>
2026-07-07 16:52:49 -0300

    atividade23

---
+++ atividade23/lista_cliente.php
@@ -0,0 +1,18 @@
+<?php 
+include "banco.php";
+
+$sql = "SELECT * FROM form";
+
+$resultado = $conexao->query($sql);
+
+
+    while($cliente = $resultado->fetch_assoc()) {
+        echo "Nome: ".$cliente['nome'] . "<br>";
+        echo "Idade: ".$cliente['idade'] . "<br>";
+        echo "Email: ".$cliente['email'] . "<br>";
+        echo "Telefone: ".$cliente['telefone'] . "<br>";
+        echo "Cidade: ".$cliente['cidade'] . "<br>";
+        
+    }
+       
+?>
